f9b62f057967b0cffc9d20f6642c96e3ef91dd3a
Giancarlo Hurtado <giancarloh@gmail.com>
2021-10-28 15:37:14 -0500

    Hasta el detalle de los post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView, BlogDetailView
 
 
 app_name = "blog"
@@ -8,4 +8,5 @@ app_name = "blog"
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='detail')
 ]
